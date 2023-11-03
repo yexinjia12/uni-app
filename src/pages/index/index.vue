9e93cea008471a0371430128d807500f80ba1b6a
wxf <wangxiaomei722@gmail.com>
2023-11-03 14:13:52 +0800

    安装uni-ui组件库

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,9 @@
 
 <template>
   <view class="index">index</view>
+  <uni-card title="基础卡片" extra="额外信息">
+    <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
+  </uni-card>
 </template>
 
 <style lang="scss">
