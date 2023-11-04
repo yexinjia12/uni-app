010e7ac6e9243106e67ba95b52c7e10758cc9497
wxf <wangxiaomei722@gmail.com>
2023-11-04 22:12:26 +0800

    首页-猜你喜欢封装

--- uni-rabbit/src/types/component.d.ts
+++ uni-rabbit/src/types/component.d.ts
@@ -3,9 +3,14 @@
  *   现调整为
  * declare module 'vue'
  */
-import 'vue'
+// import 'vue'
+import XtxSwiper from '@/components/XtxSwiper.vue'
+import XtxGuess from '@/components/XtxGuess.vue'
+
+// 添加组件类型声明
 declare module 'vue' {
   export interface GlobalComponents {
-    //
+    XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
