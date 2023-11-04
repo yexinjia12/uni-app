a31c9fa72f8863466e6fb4de59009a9cd43b86d6
wxf <wangxiaomei722@gmail.com>
2023-11-04 18:24:09 +0800

    轮播图-获取轮播图数据

--- uni-rabbit/src/pages/index/index.vue
+++ uni-rabbit/src/pages/index/index.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
-//
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+
+// 获取banner轮播图
+const homeBannerList = ref([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  homeBannerList.value = res.result
+}
+onLoad(() => getHomeBannerData())
 </script>
 
 <template>
