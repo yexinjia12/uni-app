5539aefaf7c3a5c96a0700712dc6b92a6a19a4ac
wxf <wangxiaomei722@gmail.com>
2023-11-04 22:58:07 +0800

    猜你喜欢-类型定义和列表渲染

--- uni-rabbit/src/components/XtxGuess.vue
+++ uni-rabbit/src/components/XtxGuess.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { getHomeGoodsGuessLikeAPI } from '@/services/home'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+import type { GuessItem } from '@/types/home'
 
 // 猜你喜欢 数据渲染
+const guessList = ref<GuessItem[]>([])
 const getHomeGoodsGuessLikeData = async () => {
   const res = await getHomeGoodsGuessLikeAPI()
+  guessList.value = res.result.items
 }
 onMounted(() => getHomeGoodsGuessLikeData())
 </script>
@@ -15,13 +18,12 @@ onMounted(() => getHomeGoodsGuessLikeData())
     <text class="text">猜你喜欢</text>
   </view>
   <view class="guess">
-    <navigator class="guess-item" v-for="item in 10" :key="item" :url="`/pages/goods/goods?id=4007498`">
-      <image class="image" mode="aspectFill"
-        src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_big_1.jpg"></image>
-      <view class="name"> 德国THORE男表 超薄手表男士休闲简约夜光石英防水直径40毫米 </view>
+    <navigator class="guess-item" v-for="item in guessList" :key="item.id" :url="`/pages/goods/goods?id=4007498`">
+      <image class="image" mode="aspectFill" :src="item.picture"></image>
+      <view class="name"> {{ item.name }} </view>
       <view class="price">
         <text class="small">¥</text>
-        <text>899.00</text>
+        <text>{{ item.price }}</text>
       </view>
     </navigator>
   </view>
