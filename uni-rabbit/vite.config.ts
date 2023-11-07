1307e7f5f5b9395b141bfde761cb68d365706446
wxf <wangxiaomei722@gmail.com>
2023-11-07 23:44:17 +0800

    配置alias别名路径

--- uni-rabbit/vite.config.ts
+++ uni-rabbit/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,10 @@ export default defineConfig({
     sourcemap: process.env.NODE_ENV === 'development',
   },
   plugins: [uni()],
+  resolve: {
+    // 别名
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
