a458e0620b5ac0001015430e180ed26d99c3add5
wxf <wangxiaomei722@gmail.com>
2023-11-08 16:23:46 +0800

    商品详情-轮播图交互

--- uni-rabbit/src/pages/goods/goods.vue
+++ uni-rabbit/src/pages/goods/goods.vue
@@ -19,6 +19,19 @@ const getGoodsByIdData = async () => {
 onLoad(() => {
   getGoodsByIdData()
 })
+
+// 滑动轮播图
+const currSwiperItemIndex = ref(0)
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  currSwiperItemIndex.value = ev.detail.current
+}
+// 点击轮播图-大图预览
+const onTapImage = (url: string) => {
+  uni.previewImage({
+    current: url,
+    urls: goods.value!.mainPictures,
+  })
+}
 </script>
 
 <template>
@@ -27,15 +40,15 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
+        <swiper @change="onChange" circular>
           <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+            <image @tap="onTapImage(item)" mode="aspectFill" :src="item" />
           </swiper-item>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ currSwiperItemIndex + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
