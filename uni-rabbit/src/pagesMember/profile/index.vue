91f999a04079007908626230988cea5fc592dc8b
wxf <wangxiaomei722@gmail.com>
2023-11-10 23:20:24 +0800

    个人信息-性别修改

--- uni-rabbit/src/pagesMember/profile/index.vue
+++ uni-rabbit/src/pagesMember/profile/index.vue
@@ -2,7 +2,7 @@
 import { getMemberProfileAPI, putMemberProfileAPI } from '@/services/profile'
 import { ref } from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
-import type { ProfileDetail } from '@/types/member'
+import type { ProfileDetail, Gender } from '@/types/member'
 import { useMemberStore } from '@/stores'
 
 // 获取屏幕边界到安全区域距离
@@ -50,10 +50,16 @@ const onAvatarChange = () => {
   })
 }
 
+// 修改性别
+const onGenderChange: UniHelper.RadioGroupOnChange = (ev) => {
+  profile.value.gender = ev.detail.value as Gender
+}
+
 // 个人信息-点击保存
 const onSubmit = async () => {
   const res = await putMemberProfileAPI({
     nickname: profile.value?.nickname,
+    gender: profile.value.gender,
   })
   // 修改Store昵称
   memberStore.profile.nickname = res.result.nickname
@@ -93,7 +99,7 @@ const onSubmit = async () => {
         </view>
         <view class="form-item">
           <text class="label">性别</text>
-          <radio-group>
+          <radio-group @change="onGenderChange">
             <label class="radio">
               <radio value="男" color="#27ba9b" :checked="profile?.gender === '男'" />
               男
