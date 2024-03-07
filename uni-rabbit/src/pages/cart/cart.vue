87c5c052779dffc7f32e22676722693090a8eece
wxf <wangxiaomei722@gmail.com>
2024-03-07 19:22:56 +0800

    购物车模块-底部结算信息

--- uni-rabbit/src/pages/cart/cart.vue
+++ uni-rabbit/src/pages/cart/cart.vue
@@ -64,6 +64,31 @@ const onChangeSelectedAll = () => {
   // 后端数据更新
   putMemberCartSelectedAPI({ selected: _isSelectedAll })
 }
+
+// 计算选中单品列表
+const selectedCartList = computed(() => {
+  return cartList.value.filter((item) => item.selected)
+})
+
+// 计算选中总件数
+const selectedCartListCount = computed(() => {
+  return selectedCartList.value.reduce((sum, item) => sum + item.count, 0)
+})
+
+// 计算选中总金额
+const selectedCartListMoney = computed(() => {
+  return selectedCartList.value.reduce((sum, item) => sum + item.count * item.price, 0).toFixed(2)
+})
+
+// 结算按钮交互
+const onPayment = () => {
+  if (selectedCartListCount.value === 0) {
+    return uni.showToast({
+      title: '请选择商品',
+      icon: 'none',
+    })
+  }
+}
 </script>
 
 <template>
@@ -121,9 +146,11 @@ const onChangeSelectedAll = () => {
       <view class="toolbar">
         <text @tap="onChangeSelectedAll" class="all" :class="{ checked: isSelectedAll }">全选</text>
         <text class="text">合计:</text>
-        <text class="amount">100</text>
+        <text class="amount">{{ selectedCartListMoney }}</text>
         <view class="button-grounp">
-          <view class="button payment-button" :class="{ disabled: true }"> 去结算(10) </view>
+          <view @tap="onPayment" class="button payment-button" :class="{ disabled: selectedCartListCount === 0 }">
+            去结算({{ selectedCartListCount }})
+          </view>
         </view>
       </view>
     </template>
