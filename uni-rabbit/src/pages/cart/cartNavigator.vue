388b4e188fe0dbda2725679f232ca40e8730b659
wxf <wangxiaomei722@gmail.com>
2024-03-07 20:16:31 +0800

    购物车模块-购物车普通页底部工具栏安全区适配

--- uni-rabbit/src/pages/cart/cartNavigator.vue
+++ uni-rabbit/src/pages/cart/cartNavigator.vue
@@ -1,7 +1,20 @@
 <script setup>
 import cartMain from './components/cartMain.vue'
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
+const safeBottom = safeAreaInsets?.bottom + 'px'
 </script>
 <template>
   <!-- 普通页购物车 -->
-  <cartMain></cartMain>
+  <view class="cart-navigator">
+    <cartMain></cartMain>
+  </view>
 </template>
+
+<style lang="scss">
+.cart-navigator {
+  .toolbar {
+    padding-bottom: v-bind(safeBottom);
+  }
+}
+</style>
